a1dedd3d3149ffc02eee641f83eda15aa5903f68
heyprashant <prashant.p7804@gmail.com>
2020-12-25 22:14:25 +0530

    Add search form

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
     res.render('index',{
-        title: 'Weather App',
+        title: 'Weather',
         name: 'Prashant'
     })
 } );
@@ -47,7 +47,7 @@ app.get('/weather', ( req, res) => {
             error : 'You must provide the address!'
         })
     }
-    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
         if(error) {
             return res.send({ error });
         }
